2b8b846f08bef03bbd7f963bf699631392a702e8
Max Reiter <max.m.reiter@gmail.com>
2022-04-05 11:48:23 -0700

    support before parameter

--- log.py
+++ log.py
@@ -17,7 +17,12 @@ class Log(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
-    async def log(self, ctx: commands.Context, ch: discord.TextChannel, dry: bool = False, limit: int = None):
+    async def log(self,
+                  ctx: commands.Context,
+                  ch: discord.TextChannel,
+                  after: discord.Message = None,
+                  dry: bool = False,
+                  limit: int = None):
         await ctx.send("Logging...")
 
         start = datetime.datetime.now()
@@ -29,16 +34,21 @@ class Log(commands.Cog):
             db = sqlite3.connect(f"{ch.id}.db")
             cur = db.cursor()
 
-            cur.execute("""CREATE TABLE messages (sf text, content text);""")
+            try:
+                cur.execute("""CREATE TABLE IF NOT EXISTS messages (sf text, content text);""")
+            except sqlite3.OperationalError as e:
+                log.error(e)
+                return
 
-            async for msg in ch.history(limit=limit):
+            async for msg in ch.history(limit=limit, after=after):
+                log.info(f"writing {msg.id} to database")
                 content = utils.remove_markdown(msg.clean_content)
                 cur.execute("INSERT INTO messages (sf, content) VALUES (?, ?);", (msg.id, content))
                 db.commit()
 
-        end = datetime.datetime.now()
+            db.close()
 
-        db.close()
+        end = datetime.datetime.now()
         await ctx.reply(f"Finished logging {ch.id}. Took {(end - start).total_seconds()}s")
 
 
